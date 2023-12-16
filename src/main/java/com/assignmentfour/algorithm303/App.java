b6e5bb1373652d0038f3f6f7a3de78ac27387707
aysbukre <aysbukre@gmail.com>
2023-12-16 16:55:05 +0300

    first commit

---
+++ src/main/java/com/assignmentfour/algorithm303/App.java
@@ -0,0 +1,13 @@
+package com.assignmentfour.algorithm303;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
